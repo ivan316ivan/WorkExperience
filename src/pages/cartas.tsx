e047e378fb944a0cb4271cd1d3595622a7ee7e3b
Ivan Shuvalov <ivan.shuvalov@colba.es>
2024-06-19 09:52:15 +0200

    Añadir carta

--- src/pages/cartas.tsx
+++ src/pages/cartas.tsx
@@ -1,6 +1,11 @@
+import { Carta } from "../components/carta/carta";
+
 export const Cartas = () => {
-    return (<>
-    <div>Cartas</div>
-</>
-);
+    return (
+        <>
+            <div>
+                <Carta />
+            </div>
+        </>
+    );
 };
