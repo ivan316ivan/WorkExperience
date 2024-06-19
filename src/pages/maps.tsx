71b21e6dde1714543263facb3cb092bf7ed821f8
Aitor Benítez Estruch <aitor.benitez@colba.es>
2024-06-19 14:01:05 +0200

    maps

--- src/pages/maps.tsx
+++ src/pages/maps.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import { clashOfClansDataSource } from "../dataSource/clashOfClansDataSource";
+import arena1 from "../assets/arena_goblin.webp"
+
 export const Maps = () => {
+    const [cards, setCards] = useState<any[]>([]);
+
+    useEffect(() => {
+        setCards(clashOfClansDataSource().getCards().items);
+    }, []);
+
+
     return(<>
-    <div>Maps</div>
+    <div>
+        <div className={arena1}>
+            <h3>Arena 1 {"(0-300 copas)"}</h3>
+            <img src={arena1}></img>
+            
+        </div>
+    </div>
     </>
     );
 };
