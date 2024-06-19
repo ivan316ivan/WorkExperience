de1d6a7502b6a1f243476af736b35e79ee92ab89
Aitor Benítez Estruch <aitor.benitez@colba.es>
2024-06-19 11:06:36 +0200

    credit card

--- src/components/carta/carta.tsx
+++ src/components/carta/carta.tsx
@@ -1,3 +1,10 @@
+import styles from "./carta.module.css"
+
 export const Carta = () => {
-    return <>asda</>;
+    return <>
+        <div className={styles.carta}>
+            <h4>Caballero</h4>
+            <img src="https://api-assets.clashroyale.com/cards/300/jAj1Q5rclXxU9kVImGqSJxa4wEMfEhvwNQ_4jiGUuqg.png" alt="caballero"></img>
+        </div>
+    </>;
 };
--- src/pages/cartas.tsx
+++ src/pages/cartas.tsx
@@ -1,11 +1,23 @@
 import { Carta } from "../components/carta/carta";
+import styles from "./cartas.module.css"
 
 export const Cartas = () => {
     return (
         <>
-            <div>
-                <Carta />
-            </div>
+                    <div className={styles.cartas}>
+                        <h1>CARTAS</h1>
+                    </div>
+                    <div className={styles.comon}>Comunes</div>
+                <div>
+                    <Carta />
+                    <Carta />
+                    <Carta />
+                    <Carta />
+                    <Carta />
+                    <Carta />
+                    <Carta />
+                    <Carta />
+                </div>
         </>
     );
 };
